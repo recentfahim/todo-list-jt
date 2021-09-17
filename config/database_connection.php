9eb73da4b59b1c1da316147f05c9af8c59e95f43
Fahim Ahmed <fahim@Fahims-MacBook-Pro.local>
2021-09-17 09:04:14 +0600

    initial commit

---
+++ config/database_connection.php
@@ -0,0 +1,21 @@
+<?php
+class DatabaseConnection{
+    function __construct(){
+        require_once('config.php');
+        $conntection = mysql_connect(DB_HOST, DB_USER, DB_PASSWORD);
+        mysql_select_db(DB_DATABASE, $conntection);
+        echo "Connected Successfully!!";
+
+        if(!$connection){
+            die("Can't connect to the database!!");
+        }
+
+        return $connection;
+    }
+
+    public function Close(){
+        mysql_close();
+    }
+}
+
+?>
---
+++ users/roles/list.php
@@ -0,0 +1,3 @@
+<?php
+echo "Roles list";
+?>
